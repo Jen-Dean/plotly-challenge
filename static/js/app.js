f93d54fe7205725daee341ef50a620d655b28f6a
Jen-Dean <missjenniferdean@gmail.com>
2021-01-04 17:51:24 -0500

    set up

--- static/js/app.js
+++ static/js/app.js
@@ -7,9 +7,9 @@ function buildMetadata(sample) {
 
     // Read the json data
 
-        // Parse and filter the data to get the sample's metadata
+    // Parse and filter the data to get the sample's metadata
 
-        // Specify the location of the metadata and update it
+    // Specify the location of the metadata and update it
 
 }
 
@@ -18,30 +18,39 @@ function buildCharts(sample) {
 
     // Read the json data
 
-        // Parse and filter the data to get the sample's OTU data
-        // Pay attention to what data is required for each chart
+    // Parse and filter the data to get the sample's OTU data
+    // Pay attention to what data is required for each chart
 
-        // Create bar chart in correct location
+    // Create bar chart in correct location
 
-        // Create bubble chart in correct location
-    
+    // Create bubble chart in correct location
+
 }
 
 // Define function that will run on page load
 function init() {
 
     // Read json data
+    d3.json("samples.json").then((response) => {
+        console.log(response)
+
+        response.names.forEach((item) => {
+            var option = d3.select("#selDataset").append("option");
+            option.text(item);
+            option.property("value", item);
+        });
 
         // Parse and filter data to get sample names
+        // Add dropdown option for each sample - Iteration & add to dropdown
+        // Array Name Options... 
 
-        // Add dropdown option for each sample
+        // Use first sample to build metadata and initial plots
 
-    // Use first sample to build metadata and initial plots
-
+    });
 }
 
-function optionChanged(newSample){
-
+function optionChanged(newSample) {
+    console.log(newSample);
     // Update metadata with newly selected sample
 
     // Update charts with newly selected sample
